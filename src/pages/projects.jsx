26a1bb0ddfed0d92e4fab394ce21f7de0a584312
olakunle-exxdee <saheedogunsolu2016@gmail.com>
2022-10-13 23:15:27 +0200

    changes

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -51,10 +51,7 @@ export default function Projects() {
           content="Things I’ve made trying to put my dent in the universe."
         />
       </Head>
-      <SimpleLayout
-        title="Things I’ve made trying to put my dent in the universe."
-        intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
-      >
+      <SimpleLayout title="Some of the things I’ve made.">
         <ul
           role="list"
           className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
