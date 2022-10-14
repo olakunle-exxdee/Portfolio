0cd33323ac85aac5932bcfe7638d1289d46ae2ab
olakunle-exxdee <saheedogunsolu2016@gmail.com>
2022-10-14 19:18:00 +0200

    added new project

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -253,18 +253,18 @@ export default function Home({ articles }) {
         <title>Saheed Ogunsolu, Frontend Engineer & Masters Student.</title>
         <meta
           name="description"
-          content="I'm Saheed Olakunle a Frontend Engineer/Master’s Student  based in Rouen , France. utilizing web-based technologies like HTML, CSS, Javascript, Typescript, and React to solve issues, bridging the gap, between functionality and aesthetics"
+          content="I'm Saheed Olakunle, a Frontend Engineer/Master’s Student  based in Rouen, France. utilizing web-based technologies like HTML, CSS, Javascript, Typescript, and React to solve issues, bridging the gap, between functionality and aesthetics"
         />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Saheed Ogunsolu , Frontend Engineer , Masters Student and PartTime
-            Footballer.
+            Saheed Ogunsolu,
+            <br /> Frontend Engineer, Masters Student and PartTime Footballer.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I am Saheed Olakunle a Frontend Engineer and Master’s Student at
-            ESIGELEC based in Rouen , France. I utilize web-based technologies
+            I am Saheed Olakunle, a Frontend Engineer and Master’s Student at
+            ESIGELEC based in Rouen, France. I utilize web-based technologies
             like HTML, CSS, Javascript, Typescript, and React to solve issues,
             bridging the gap, between functionality and aesthetics
           </p>
