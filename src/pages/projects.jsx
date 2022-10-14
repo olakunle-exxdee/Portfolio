b37c84ccf578084cea55cb281009860c974e0c4f
olakunle-exxdee <saheedogunsolu2016@gmail.com>
2022-10-14 19:38:22 +0200

    added new project

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -52,7 +52,7 @@ export default function Projects() {
           content="Things I’ve made trying to put my dent in the universe."
         />
       </Head>
-      <SimpleLayout title="Projects.">
+      <SimpleLayout title="Project">
         <ul
           role="list"
           className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
